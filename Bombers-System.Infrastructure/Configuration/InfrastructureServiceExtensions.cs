b3eb42b5625c67275a2fd3b9912eca01059176f9
Carlos <carlos.huamani.pi@tecsup.edu.pe>
2026-05-31 22:51:30 -0500

    feat: add infrastructure service registration extension

---
+++ Bombers-System.Infrastructure/Configuration/InfrastructureServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Bombers_System.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bombers_System.Infrastructure.Configuration;
+
+public static class InfrastructureServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Database Connection
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            options.UseNpgsql(connectionString);
+        });
+        
+        // Services Register
+        // services.AddScoped<IUnitOfWork, UnitOfWork>();
+        // services.AddScoped<IUserRepository, UserRepository>();
+        // services.AddScoped<IAuthService, AuthService>();
+        
+        return services;
+    }
+}
