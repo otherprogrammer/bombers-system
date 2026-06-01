fb3ab23de540ab8b6eccae001dc5fb054dd6881b
Carlos <carlos.huamani.pi@tecsup.edu.pe>
2026-05-31 23:18:12 -0500

    feat: add service registration extensions on API shape with Authentication.JwtBearer

---
+++ Bombers-System/Configuration/ServiceRegistrationExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using Bombers_System.Infrastructure.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace Bombers_System.Configuration;
+
+public static class ServiceRegistrationExtensions
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Register Infrastructure Services
+        services.AddInfrastructureServices(configuration);
+        
+        // Enable Controllers
+        services.AddControllers();
+        
+        // Add Authentication Services
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"] 
+                        ?? throw new InvalidOperationException("JWT SecretKey is missing in configuration."))),
+                };
+            });
+        
+        // Enable Swagger
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Bombers Systema API", Version = "v1" });
+
+            // Security Scheme
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using Bearer scheme. \r\n\r\n" +
+                              "Enter 'Bearer' [space] and then your token in the text box.\r\n\r\n" +
+                              "Example: \"Bearer eyJhbGciOiJIUzI1Ni...\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            // Security Requirements
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+        
+        return services;
+    }
+}
