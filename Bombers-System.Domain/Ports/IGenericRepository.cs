51cee1df52cb7b789f39904bf1572f45f9c7bc33
Carlos <carlos.huamani.pi@tecsup.edu.pe>
2026-06-01 17:27:04 -0500

    feat: implement GenericRepository and its port

---
+++ Bombers-System.Domain/Ports/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Bombers_System.Domain.Ports;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task AddAsync(T entity);
+    Task Update(T entity);
+    Task DeleteAsync(int id);
+}
---
+++ Bombers-System.Infrastructure/Adapters/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Bombers_System.Domain.Ports;
+using Bombers_System.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bombers_System.Infrastructure.Adapters;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _context;
+    
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().AsNoTracking().ToListAsync();
+    }
+    
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+    }
+
+    public Task Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+        if (entity != null)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+    }
+}
