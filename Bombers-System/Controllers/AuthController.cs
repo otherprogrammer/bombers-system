42b102a441c4403147ca46a1142a805b386ff2ef
Carlos <carlos.huamani.pi@tecsup.edu.pe>
2026-06-04 11:17:03 -0500

    feat: add authorize in auth controller endpoints

--- Bombers-System/Controllers/AuthController.cs
+++ Bombers-System/Controllers/AuthController.cs
@@ -1,9 +1,11 @@
 ﻿using Bombers_System.Application.UseCases.AuthUseCases.Command;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bombers_System.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/auth")]
 public class AuthController : ControllerBase
@@ -16,6 +18,7 @@ public class AuthController : ControllerBase
     }
 
     [HttpPost("register")]
+    [AllowAnonymous]
     public async Task<IActionResult> Register([FromBody] RegisterUserCommand command, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(command, cancellationToken);
@@ -23,12 +26,14 @@ public class AuthController : ControllerBase
     }
         
     [HttpPost("login")]
+    [AllowAnonymous]
     public async Task<IActionResult> Login([FromBody] LoginUserCommand command, CancellationToken cancellationToken)
     {
         return Ok(await _mediator.Send(command, cancellationToken));
     }
 
     [HttpPost("refresh-token")]
+    [AllowAnonymous]
     public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenCommand command, CancellationToken cancellationToken)
     {
         return Ok(await _mediator.Send(command, cancellationToken));
