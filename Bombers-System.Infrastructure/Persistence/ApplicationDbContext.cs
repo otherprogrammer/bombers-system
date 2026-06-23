e28688043ae5855c04240909387778ad13db4e64
CarlosHuamaniTec <carlos.huamani.pi@gmail.com>
2026-06-23 13:20:04 -0500

    refactor: remove old db credentials from dbcontext

--- Bombers-System.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Bombers-System.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -39,11 +39,7 @@ public partial class ApplicationDbContext : DbContext
     public virtual DbSet<UserToken> UserTokens { get; set; }
 
     public virtual DbSet<Vehicle> Vehicles { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=aws-1-us-east-1.pooler.supabase.com;Database=postgres;Username=postgres.layhndjrqrhzzbzrobif;Password=t7f3a4KlKGR2bvQV;Port=5432;SSL Mode=Require;Trust Server Certificate=true;", x => x.UseNetTopologySuite());
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
